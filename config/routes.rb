47a0c58c3d4f7e95cd2127f9f88619df36b259cf
m1khah <alexeyenko92@gmail.com>
2016-08-21 09:17:03 +0300

    Minor improvements,clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'lists#index'
+  resources :lists
+  resources :tasks do
+    collection { post :sort }
+    member do
+      post 'done'
+    end
+  end
 end
